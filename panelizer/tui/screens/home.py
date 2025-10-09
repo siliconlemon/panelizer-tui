76dc398537801e9bc4e1417adac9045bf342f48a
siliconlemon <ondrej.wiener@gmail.com>
2025-10-09 13:38:57 +0200

    Made the switch button more responsive

--- panelizer/tui/screens/home.py
+++ panelizer/tui/screens/home.py
@@ -203,6 +203,10 @@ class SwitchButton(Widget):
                 border: round $accent;
             }
             
+             &.--hover:focus-within {
+                border: round $secondary;
+             }
+            
             &.--toggled Label {
                 color: $success-lighten-1;
             }
