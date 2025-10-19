318469aaac44da42cfb1259e7ac7fb959216b7ef
siliconlemon <ondrej.wiener@gmail.com>
2025-10-20 00:38:02 +0200

    Fixed a CommandPalette CSS glitch

--- textual_neon/app/neon_app.py
+++ textual_neon/app/neon_app.py
@@ -63,7 +63,10 @@ class NeonApp(App[Any]):
         Horizontal#--input {
             max-height: 3;
             margin: 0;
-            border: round $accent 70%;
+            border-top: round $accent 70%;
+            border-bottom: none !important;
+            border-left: round $accent 70%;
+            border-right: round $accent 70%;
             visibility: visible;
         }
         Vertical#--results {
@@ -84,7 +87,7 @@ class NeonApp(App[Any]):
         border: none !important;
         margin: 0;
         & > .option-list--option {
-            padding: 0 1 0 1;
+            padding: 0 1 0 1 !important;
         }
     }
     """
